0850c5d84dcc90cd409c6c21423f29abef18b833
gpi <gpimpale29@gmail.com>
2020-07-22 19:55:56 -0700

    removed super ez to implement headers

--- bootstrap1/comlib/com_writer_os_errstream.c
+++
@@ -1,7 +0,0 @@
-#include "com_writer_os_errstream.h"
-#include "com_writer_fn.h"
-#include "com_os_iostream.h"
-
-com_writer com_writer_os_errstream_create(void) {
-	return com_writer_fn_create(com_os_iostream_write_err);
-}
--- bootstrap1/comlib/com_writer_os_outstream.c
+++
@@ -1,7 +0,0 @@
-#include "com_writer_os_outstream.h"
-#include "com_writer_fn.h"
-#include "com_os_iostream.h"
-
-com_writer com_writer_os_outstream_create(void) {
-	return com_writer_fn_create(com_os_iostream_write_out);
-}

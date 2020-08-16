089f1cc090425872b7bd9e528444cea2eecc6b43
NitishReuben <nitishreuben2600@gmail.com>
2020-08-16 12:31:38 -0700

    fixed syntax errors

--- bootstrap2/grammar_peg.py
+++ bootstrap2/grammar_peg.py
@@ -3,19 +3,19 @@ from __future__ import absolute_import, unicode_literals, print_function
 import os
 from arpeggio.peg import ParserPEG
 from arpeggio import visit_parse_tree
-from achernar import CalcVisitor
+from achernar import GrammarVisitor
 
 def main(debug=False):
 
     # Grammar is defined using textual specification based on PEG language.
     # Load grammar form file.
-    calc_grammar = open(os.path.join(os.path.dirname(__file__), 'calc.peg'),
+    grammar = open(os.path.join(os.path.dirname(__file__), '../grammar.peg'),
                         'r').read()
 
-    # First we will make a parser - an instance of the calc parser model.
+    # First we will make a parser - an instance of the grammar model.
     # Parser model is given in the form of PEG notation therefore we
-    # are using ParserPEG class. Root rule name (parsing expression) is "calc".
-    parser = ParserPEG(calc_grammar, "calc", debug=debug)
+    # are using ParserPEG class. Root rule name (parsing expression) is "Root".
+    parser = ParserPEG(grammar, "Root", debug=debug)
 
     # An expression we want to evaluate
     input_expr = "3*(4+2)"
@@ -27,10 +27,10 @@ def main(debug=False):
     # visit_parse_tree will start semantic analysis.
     # In this case semantic analysis will evaluate expression and
     # returned value will be evaluated result of the input_expr expression.
-    result = visit_parse_tree(parse_tree, CalcVisitor(debug=debug))
+    # result = visit_parse_tree(parse_tree, GrammarVisitor(debug=debug))
 
 
-    print("{} = {}".format(input_expr, result))
+    # print("{} = {}".format(input_expr, result))
 
 if __name__ == "__main__":
     # In debug mode dot (graphviz) files for parser model

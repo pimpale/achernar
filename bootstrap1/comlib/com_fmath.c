d02d15830d3fa23ad1424f67d344ed3836014380
gpi <gpimpale29@gmail.com>
2020-08-28 13:54:32 -0700

    compiles

---
+++ bootstrap1/comlib/com_fmath.c
@@ -0,0 +1,6 @@
+#include "com_fmath.h"
+#include "com_assert.h"
+
+f64 attr_NORETURN com_fmath_f64_pow(attr_UNUSED f64 base, attr_UNUSED f64 exp ){
+  com_assert_unreachable_m("this method is unimplemented");
+}

4a896c885f7b24588efbd201da965a0cffd42e25
gpi <gpimpale29@gmail.com>
2022-04-14 15:42:34 -0700

    think about mir

--- src/mirbuilder.rs
+++ src/mirbuilder.rs
@@ -7,28 +7,12 @@ use num_rational::BigRational;
 use std::alloc::Allocator;
 
 use super::mir;
-/*
-pub fn eval<'ast, 'hir, 'mir, A: Allocator, HA: Allocator>(
-  hir: &'mir mir::Expr<'ast, 'hir, 'mir, A, HA>,
-  allocator: &'mir Bump,
-  mut dlogger: DiagnosticLogger,
-) {
-}
 
-pub fn construct_mir_pat<'ast, 'hir, 'mir, HirAllocator: Allocator>(
-  hir: &'hir hir::Pat<'hir, 'ast, HirAllocator>,
+pub fn construct_mir<'ast, 'hir, 'mir, HirAllocator: Clone + Allocator>(
+  hir: &'hir hir::ValExpr<'hir, 'ast, HirAllocator>,
   allocator: &'mir Bump,
   mut dlogger: DiagnosticLogger,
-) {
-}
-
-pub fn construct_mir<'ast, 'hir, 'mir, HirAllocator: Allocator>(
-  hir: &'hir hir::Expr<'hir, 'ast, HirAllocator>,
-  allocator: &'mir Bump,
-  mut dlogger: DiagnosticLogger,
-) -> BasicBlock{
-  match (hir) {
-    None => No
+) -> mir::BasicBlock {
+  match hir {
   }
 }
-*/
